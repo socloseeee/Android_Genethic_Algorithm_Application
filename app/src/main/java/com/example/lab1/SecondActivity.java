df2b4f631e96ae39b91ac4e5c7964fc6214a2b9b
socloseeee <65871712+socloseeee@users.noreply.github.com>
2023-02-14 09:17:51 +0300

    update

--- app/src/main/java/com/example/lab1/SecondActivity.java
+++ app/src/main/java/com/example/lab1/SecondActivity.java
@@ -1,20 +1,21 @@
 package com.example.lab1;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.view.View;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_second);
     }
 
-    public void startNewActivity(View v) {
-        Intent intent = new Intent(this, SecondActivity.class);
+    public void goBack(View v) {
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 }
